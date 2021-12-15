e0bd93750703a65f690460aca5ccd606ac17d3f5
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-14 17:19:13 -0800

    character names rendering

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import CharacterList from '../../components/Characters/CharacterList';
+import { fetchCharacters } from '../../services/characters';
 
 export default function Characters() {
+  const [characters, setCharacters] = useState([]);
+  const [race, setRace] = useState('All');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchCharacters(race, query);
+      setCharacters(data);
+      console.log(data);
+    };
+    fetchData();
+  }, [race, query]);
   return (
     <div>
-      <h1>Characters</h1>
+      {characters.map((character) => {
+        return <CharacterList key={character.id} {...character} />;
+      })}
     </div>
   );
 }
