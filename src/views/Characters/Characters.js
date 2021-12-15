3c7bff753c7820a8b4fabd9cf7ceec1b60c05024
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-14 17:34:02 -0800

    race dropdown function addeD

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -16,10 +16,23 @@ export default function Characters() {
     fetchData();
   }, [race, query]);
   return (
-    <div>
-      {characters.map((character) => {
-        return <CharacterList key={character.id} {...character} />;
-      })}
-    </div>
+    <>
+      <div>
+        <select value={race} onChange={(e) => setRace(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Dwarf">Dwarf</option>
+          <option value="Elf">Elf</option>
+          <option value="Hobbit">Hobbit</option>
+          <option value="Human">Human</option>
+          <option value="Maiar">Maiar</option>
+          <option value="Orc">Orc</option>
+        </select>
+      </div>
+      <div>
+        {characters.map((character) => {
+          return <CharacterList key={character.id} {...character} />;
+        })}
+      </div>
+    </>
   );
 }
